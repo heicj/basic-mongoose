64e6bc58e78ac586695454b2fcc396d19edd3379
Charlie <heinercj@gmail.com>
2018-04-17 17:17:46 -0700

    added team model and created valid model test

---
+++ test/unit/Team.test.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+const Team = require('../../lib/models/BBTeam');
+
+describe('Team model', () => {
+    
+    it('valid team model', () => {
+        const data = {
+            teamName: 'Newbies',
+            coach: 'Shooter',
+            stadium: {
+                name: 'Stadium Name For Sale',
+                capacity: 10000
+            },
+            location: {
+                state: 'OR',
+                city: 'Portland'
+            },
+            players: ['Player1', 'Player2', 'Player3']
+        };
+    
+        const team = new Team(data);
+    
+        assert.deepEqual(team.toJSON(), {
+            _id: team._id,
+            ...data
+        });
+
+
+    });
+
+});
